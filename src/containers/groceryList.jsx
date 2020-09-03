5490480290c401e9e7e1af20cf304e6457e5fca8
Wrushasen Dakhane <wrushu2004@gmail.com>
2020-09-03 16:23:29 -0700

    added reducer for handling http related state

--- src/containers/groceryList.jsx
+++ src/containers/groceryList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useReducer } from "react";
+import React, { useCallback, useReducer } from "react";
 import { Fragment } from "react";
 import AddItem from "./addItem";
 import SearchItem from "../components/searchItem";
@@ -7,63 +7,79 @@ import axios from "../axios-grocery";
 import GroceryItem from "../components/groceryItem";
 import ErrorModal from "../components/errorModal";
 
-const groceryItemsReducer = (groceryItems, action) => {
+const groceryItemsReducer = (prevState, action) => {
   switch (action.type) {
     case "SET":
       return action.groceryItems;
     case "ADD":
-      return [...groceryItems, action.groceryItem];
+      return [...prevState, action.groceryItem];
     case "REMOVE":
-      return groceryItems.filter((item) => item.id !== action.id);
+      return prevState.filter((item) => item.id !== action.id);
     default:
       throw new Error("Should not get here!");
   }
 };
 
+const httpReducer = (prevState, action) => {
+  switch (action.type) {
+    case "HTTP_SEND":
+      return { loading: true, error: null };
+    case "HTTP_RESPONSE":
+      return { ...prevState, loading: false };
+    case "HTTP_ERROR":
+      return { loading: false, error: action.errorMessage };
+    case "CLEAR_ERROR":
+      return { ...prevState, error: null };
+    default:
+      throw new Error("Should not be here!");
+  }
+};
+
 function GroceryList(props) {
   const [groceryItems, dispatch] = useReducer(groceryItemsReducer, []);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
+  const [httpState, dispatchHttp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
   const loadItems = useCallback((groceryItems) => {
     dispatch({ type: "SET", groceryItems: groceryItems });
   }, []);
 
   const addItem = async (item) => {
     try {
-      setLoading(true);
+      dispatchHttp({ type: "HTTP_SEND" });
       const { data } = await axios.post("/groceryItems.json", item);
       item["id"] = data.name;
       dispatch({ type: "ADD", groceryItem: item });
-      setLoading(false);
+      dispatchHttp({ type: "HTTP_RESPONSE" });
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatchHttp({ type: "HTTP_ERROR", errorMessage: error.message });
     }
   };
 
   const removeItem = async (id) => {
     try {
-      setLoading(true);
+      dispatchHttp({ type: "HTTP_SEND" });
       await axios.delete(`/groceryItems/${id}.json`);
       dispatch({ type: "REMOVE", id: id });
-      setLoading(false);
+      dispatchHttp({ type: "HTTP_RESPONSE" });
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatchHttp({ type: "HTTP_ERROR", errorMessage: error.message });
     }
   };
 
   const clearError = () => {
-    setError(null);
+    dispatch({ type: "CLEAR_ERROR" });
   };
   return (
     <Fragment>
-      {error && <ErrorModal clearError={clearError}>{error}</ErrorModal>}
+      {httpState.error && (
+        <ErrorModal clearError={clearError}>{httpState.error}</ErrorModal>
+      )}
       <div className="row">
         <div className="col">
           <div className="shadow p-3 mb-2 bg-white rounded">
-            <AddItem onAdd={addItem} loading={loading} />
+            <AddItem onAdd={addItem} loading={httpState.loading} />
           </div>
         </div>
       </div>
